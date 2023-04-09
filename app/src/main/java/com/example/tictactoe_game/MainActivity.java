b6c10e4e437877a577013278ae315d27c162aae1
NOUMAN <naumanhanif99@gmail.com>
2023-04-09 18:59:09 +0500

    tictactoe

---
+++ app/src/main/java/com/example/tictactoe_game/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.tictactoe_game;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+
+    // Player representation
+    // 0 - X
+    // 1 - O
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    // State meanings:
+    // 0 - X
+    // 1 - O
+    // 2 - Null
+    int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+    public static int counter = 0;
+
+    //function alwos player to tap the screen to play the game
+    public void playerTap(View view) {
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+
+        //game resets if the boxes are full or the player wins
+        if (!gameActive) {
+            gameReset(view);
+        }
+
+        if (gameState[tappedImage] == 2) {
+            counter++;
+
+            if (counter == 9) {
+                gameActive = false;
+            }
+
+            //points the first move of the player
+            gameState[tappedImage] = activePlayer;
+
+            img.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                // set the image of x
+                img.setImageResource(R.drawable.cross);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+
+                // change the status
+                status.setText("O's Turn - Tap to play");
+            } else {
+                // set the image of o
+                img.setImageResource(R.drawable.circle);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+
+                // change the status
+                status.setText("X's Turn - Tap to play");
+            }
+            img.animate().translationYBy(1000f).setDuration(300);
+        }
+        int flag = 0;
+        // Check if any player has won
+        for (int[] winPosition : winPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]] != 2) {
+                flag = 1;
+
+                String winnerStr;
+
+                gameActive = false;
+                if (gameState[winPosition[0]] == 0) {
+                    winnerStr = "X has won";
+                } else {
+                    winnerStr = "O has won";
+                }
+                // Update the status bar for winner announcement
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+            }
+        }
+        // set the status if the match draw
+        if (counter == 9 && flag == 0) {
+            TextView status = findViewById(R.id.status);
+            status.setText("Match Draw");
+        }
+    }
+
+    // reset the game
+    public void gameReset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+        // remove all the images from the boxes inside the grid
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
